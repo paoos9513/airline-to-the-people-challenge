f0c57ea57e989b78b15ad39fc85ca06265f72391
Paola Osorio <paoos9513@gmail.com>
2023-09-28 17:10:32 -0500

    restructured the architecture

---
+++ src/app/shared/component/banner-with-action/banner-with-action.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-with-action',
+  templateUrl: './banner-with-action.component.html',
+  styleUrls: ['./banner-with-action.component.sass']
+})
+export class BannerWithActionComponent {
+
+}
